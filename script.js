7bf8702a83094c2d1793f99795f7d144f445fbee
Stefanos <ecomav25@gmail.com>
2026-09-09 18:36:49 +0300

    Add initial implementation of Weather App with HTML, CSS, and JavaScript
    - Created index.html for the app structure including search and result screens.
    - Developed script.js to handle API calls and display weather data.
    - Styled the app using style.css for a responsive and visually appealing layout.

---
+++ script.js
@@ -0,0 +1,94 @@
+const searchScreen = document.querySelector(".search-screen");
+const resultScreen = document.querySelector(".result-screen");
+const errorScreen = document.querySelector(".error-screen");
+
+const searchCityBtn = document.getElementById("search-city-btn");
+const searchAgainBtn = document.querySelectorAll(".search-again-btn");
+const userInput = document.getElementById("city-input");
+
+const cityName = document.getElementById("city-name");
+const weatherStatus = document.getElementById("weather-status");
+const weatherIcon = document.getElementById("weather-icon");
+const temperature = document.getElementById("temperature");
+const feelsLike = document.getElementById("feels-like");
+const windSpeed = document.getElementById("wind-speed");
+const windDirection = document.getElementById("wind-direction-arrow");
+
+
+async function getWeatherData(city) {
+    const apiKey = "66d81ebba64d347044f13d4afb688cdd";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+        
+        return data;
+
+    } catch (error) {
+        console.error("Error getting weather data: ", error.message);
+    }
+}
+
+async function displayWeather(city) {
+    const data = await getWeatherData(city);
+
+    if (!data) {
+        resultScreen.classList.add("hidden");
+        errorScreen.classList.remove("hidden");
+        return;
+    }
+
+    cityName.textContent = data.name;
+    weatherStatus.textContent = data.weather[0].main;
+    temperature.textContent = data.main.temp;
+    feelsLike.textContent = data.main.feels_like;
+    windSpeed.textContent = data.wind.speed;
+    windDirection.style.transform = `rotate(${data.wind.deg}deg)`;
+
+    switch(data.weather[0].main) {
+        case "Clear":
+            weatherIcon.innerHTML = `<i class="fa-regular fa-sun" style="color: yellow;"></i>`;
+            break;
+        case "Clouds":
+            weatherIcon.innerHTML = `<i class="fa-regular fa-cloud"></i>`;
+            break;
+        case "Rain":
+        case "Drizzle":
+            weatherIcon.innerHTML = `<i class="fa-solid fa-cloud-rain"></i>`;
+            break;
+        case "Thunderstorm": 
+            weatherIcon.innerHTML = `<i class="fa-solid fa-bolt-lightning"></i>`;
+            break;
+        case "Snow":
+            weatherIcon.innerHTML = `<i class="fa-regular fa-snowflake"></i>`;
+            break;
+        default:
+            weatherIcon.innerHTML = `<i class="fa-solid fa-cloud-sun"></i>`;
+    }
+}
+
+searchCityBtn.addEventListener("click", () => {
+    if (userInput.value === "") {
+        alert("Please Enter a City");
+        return;
+    }
+    searchScreen.classList.add("hidden");
+    resultScreen.classList.remove("hidden");
+    displayWeather(userInput.value.toLowerCase());
+
+});
+
+searchAgainBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        searchScreen.classList.remove("hidden");
+        resultScreen.classList.add("hidden");
+        errorScreen.classList.add("hidden");
+    });
+});
